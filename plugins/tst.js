4482026ad3b8a8d76f6197dda20a9cc05723660c
Aminwsf <154097506+Aminwsf@users.noreply.github.com>
2024-08-17 03:44:31 +0100

    Update tst.js

--- plugins/tst.js
+++ plugins/tst.js
@@ -1,10 +1,14 @@
 const handler = async (m, { conn, usedPrefix, command, sender }) => {
-    // Your logic here
+    
+    if (m.isOwner) return conn.sendMessage(m.chat, { text: 'Owner\'s Own'})
+        // Your logic here
     
     await conn.sendMessage(m.sender, { text: `sender: ${m.sender}\nchat: ${m.chat}\nisOwner: ${m.isOwner}\nusedPrefix: ${usedPrefix}\ncommand: ${command}\ntext: ${m.text}\nchat type: ${m.isGroup ? 'in group' : 'in private chat'}\nfromMe: ${m.fromMe}` });
 
 };
 
 handler.command = /^(test)$/i;  // Regex for the command
+handler.tags = ["owner"];
+handler.cmd = ["test"]
 
 module.exports = { handler };
